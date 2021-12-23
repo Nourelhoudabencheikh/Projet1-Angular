542f95cc5d5e575bb86d77b2261f63e69dc1ed05
ben cheikh nour elhouda <lhoudanour584@gmail.com>
2021-12-23 19:33:33 +0100

    projet1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PanierService } from './services/panier.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fullprojet';
+  logo = 'assets/logo2.png';
+  panier = 0;
+
+  constructor(private panierService: PanierService) {
+  	this.panier = this.panierService.getCartCount();
+  }
+
+  ngOnInit(): void {
+  	setInterval(() => {
+      this.panier = this.panierService.getCartCount();
+      console.log("nombre produits dans le panier : ",this.panier);
+    }, 200);
+  }
+
+
 }
